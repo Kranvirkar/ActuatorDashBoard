b4c84de257ced26e683847299ef64e5337f11b48
Kranvirkar <144894911+Kranvirkar@users.noreply.github.com>
2024-09-26 14:52:50 +0530

    Added Actuator Dependency

--- src/main/java/com/dashboard/actuatordashboard/ActuatorDashBoardApplication.java
+++ src/main/java/com/dashboard/actuatordashboard/ActuatorDashBoardApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActuatorDashBoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActuatorDashBoardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorDashBoardApplication.class, args);
+    }
 
 }
