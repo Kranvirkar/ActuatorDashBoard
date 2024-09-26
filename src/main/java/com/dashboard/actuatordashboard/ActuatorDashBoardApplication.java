62222688688e358f549143c4562651b53588e58f
Kranvirkar <144894911+Kranvirkar@users.noreply.github.com>
2024-09-26 14:51:52 +0530

    initial commit

---
+++ src/main/java/com/dashboard/actuatordashboard/ActuatorDashBoardApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.actuatordashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorDashBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorDashBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dashboard/actuatordashboard/ActuatorDashBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.actuatordashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorDashBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
